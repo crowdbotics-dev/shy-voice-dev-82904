67761ee2b1095fa1c5fd106117ae4848f480246f
Crowdbotics <engineering@crowdbotics.com>
2023-05-13 03:43:06 +0000

    Initial commit.

---
+++ backend/shy_voice_dev_82904/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shy_voice_dev_82904 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shy_voice_dev_82904.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-voice-dev-82904",
+  url: "https://shy-voice-dev-82904.botics.co",
+  api: "https://shy-voice-dev-82904.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
